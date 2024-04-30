5ccf7c90d38a4caf21d69252cd35e978892a06b2
Ilyass Serghini <I.Sergh@hotmail.com>
2024-04-30 18:31:36 -0400

    Seperated the project into multiple files to help with modular and maintainability

---
+++ Api_calls_swiftUI/GitHubErrors.swift
@@ -0,0 +1,8 @@
+//
+//  GitHubErrors.swift
+//  Api_calls_swiftUI
+//
+//  Created by ilyass Serghini on 2024-04-30.
+//
+
+import Foundation
---
+++ Api_calls_swiftUI/networkCallsFunction.swift
@@ -0,0 +1,8 @@
+//
+//  networkCallsFunction.swift
+//  Api_calls_swiftUI
+//
+//  Created by ilyass Serghini on 2024-04-30.
+//
+
+import Foundation
