9bbb5124ae3b296b804d0da30ee3dc6994dd7737
Ilyass Serghini <I.Sergh@hotmail.com>
2024-04-29 21:37:18 -0400

    Initial Commit

---
+++ Api_calls_swiftUI/Api_calls_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Api_calls_swiftUIApp.swift
+//  Api_calls_swiftUI
+//
+//  Created by ilyass Serghini on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Api_calls_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Api_calls_swiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Api_calls_swiftUI
+//
+//  Created by ilyass Serghini on 2024-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
