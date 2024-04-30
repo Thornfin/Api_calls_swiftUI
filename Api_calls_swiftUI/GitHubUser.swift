c2a3d631df460bbc5e74f9124989621d4fdad5c5
Ilyass Serghini <I.Sergh@hotmail.com>
2024-04-30 18:40:35 -0400

    Seperated the project into files to keep it modular and maintainable

--- Api_calls_swiftUI/GitHubUser.swift
+++ Api_calls_swiftUI/GitHubUser.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct GitHubUser: Codable {
+    let login: String
+    let avatarUrl: String
+    let bio: String
+    
+}
--- Api_calls_swiftUI/networkCallsFunction.swift
+++ Api_calls_swiftUI/networkCallsFunction.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+func getUser() async throws -> GitHubUser {
+    let endpoint = "https://api.github.com/users/facebook"
+    
+    guard let url = URL(string: endpoint) else {
+        throw GHError.invalidURL
+    }
+    
+    let (data, response) = try await URLSession.shared.data(from: url)
+    
+    guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+        throw GHError.invalidResponse
+    }
+    
+    do{
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return try decoder.decode(GitHubUser.self, from: data)
+    } catch {
+        throw GHError.invalidData
+    }
+}
